06d9492c87e3b3e6f05a066659f7449ebc04fd47
Balinca Gabriel Alin <balinca.gabriel.alin@gmail.com>
2023-07-05 13:27:53 +0300

    second commit - revert to a simple example

---
+++ Controllers/LicensingController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Http;
+
+namespace WebApiTestNetFramwork.Controllers
+{
+    public class LicensingController : ApiController
+    {
+        [HttpGet]
+        [Route("api/Licensing/Details")]
+        public IHttpActionResult GetDetails()
+        {
+            return Ok("Test from visual studiop");
+        }
+
+        [HttpPost]
+        [Route("api/Licensing/Details")]
+        public IHttpActionResult PostDetails(string message)
+        {
+            return Ok(message);
+        }
+    }
+}
